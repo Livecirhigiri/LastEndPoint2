5b084da4bbf84d29aec1ffeceb9d88b0b05dcb3a
live Cirhigiri <cimancirh@gmail.com>
2019-01-03 22:01:21 +0200

    questions test

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ const bodyParser = require("body-parser");
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-const userRoutes = require("./api/routes/user");
+/*const userRoutes = require("./api/routes/user");
 
 app.use("/user", userRoutes);
 const meetUpRoutes = require("./api/routes/meetUp");
 
 app.use("/meetUp", meetUpRoutes);
+const rsvpRoutes = require("./api/routes/rsvp");
+
+app.use("/rsvp", rsvpRoutes); */
 
 const QuestionRoutes = require("./api/routes/Question");
 
 app.use("/Question", QuestionRoutes);
-// appel functions
-const rsvpRoutes = require("./api/routes/rsvp");
-
-app.use("/rsvp", rsvpRoutes);
+// appel function
 
 app.use((req, res, next) => {
   const error = new Error("not found ");
